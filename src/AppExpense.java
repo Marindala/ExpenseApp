a943f9e23f75d7e3f1616c829ede8e977187f34d
Marina <95050756+Marindala@users.noreply.github.com>
2023-12-28 05:04:35 -0300

    first commit

---
+++ src/AppExpense.java
@@ -0,0 +1,38 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class AppExpense {
+    public static void main(String[] args) {
+         String DB_DRIVER = "org.h2.Driver";
+         String DB_CONNECTION = "jdbc:h2:~/test";
+         String DB_USER = "sa";
+         String DB_PASSWORD = "";
+        try {
+            Class.forName(DB_DRIVER); // Carga explícitamente el controlador JDBC
+            // Establecer la conexión con la base de datos
+            Connection connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+
+            // Crear una declaración SQL
+            Statement statement = connection.createStatement();
+
+            // Crear la tabla
+            String createTableQuery = "CREATE TABLE IF NOT EXISTS usuarios (id INT PRIMARY KEY, nombre VARCHAR(50))";
+            statement.executeUpdate(createTableQuery);
+
+            // Insertar registros
+            String insertQuery = "INSERT INTO usuarios VALUES (1, 'John Doe'), (2, 'Jane Smith')";
+            statement.executeUpdate(insertQuery);
+
+            // Cerrar la conexión
+            statement.close();
+            connection.close();
+
+            System.out.println("Registros insertados con éxito en la tabla 'usuarios'.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/config/JdbcConfiguration.java
@@ -0,0 +1,43 @@
+package main.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.sql.Statement;
+import java.sql.SQLException;
+
+public class JdbcConfiguration {
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:~/test";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+            System.out.println("Conexión a la base de datos establecida con éxito.");
+            // Crear una declaración SQL
+            //Statement statement = connection.createStatement();
+            // Crear la tabla
+            //String createTableQuery = "CREATE TABLE IF NOT EXISTS gastos (id INT PRIMARY KEY, nombre VARCHAR(50))";
+            //statement.executeUpdate(createTableQuery);
+            // Insertar registros
+            //String insertQuery = "INSERT INTO usuarios VALUES (1, 'comida'), (2, 'vestimenta')";
+           // statement.executeUpdate(insertQuery);
+            // Cerrar la conexión
+            //statement.close();
+            //connection.close();
+            //return connection;
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+
+        }
+        return connection;
+    }
+
+
+}
+
