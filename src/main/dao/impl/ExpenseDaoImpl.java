f8eaaa083fd924f8932cb656e17ef3ab6a216765
Marina <95050756+Marindala@users.noreply.github.com>
2024-01-03 01:04:09 -0300

    test database

---
+++ src/main/dao/impl/ExpenseDaoImpl.java
@@ -0,0 +1,10 @@
+package main.dao.impl;
+
+import main.dao.ExpenseDao;
+
+public class ExpenseDaoImpl implements ExpenseDao {
+   // @Override
+    //public List<ExpenseDao> getAll() throws DAOException {
+        //return null;
+    }
+}
