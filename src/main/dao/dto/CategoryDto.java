031a8da532ce1efd00627a04329acf64db53a0a8
Marina <95050756+Marindala@users.noreply.github.com>
2024-01-03 03:24:45 -0300

    create CategoryDto

---
+++ src/main/dao/dto/CategoryDto.java
@@ -0,0 +1,48 @@
+package main.dao.dto;
+
+public class CategoryDto {
+    private int id;
+    private String name;
+    private int operationId;
+
+    public CategoryDto() {
+    }
+
+    public CategoryDto(int id, String name, int operationId) {
+        this.id = id;
+        this.name = name;
+        this.operationId = operationId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOperationId() {
+        return operationId;
+    }
+
+    public void setOperationId(int operationId) {
+        this.operationId = operationId;
+    }
+
+    @Override
+    public String toString() {
+        return "Category {" +
+                "name ='" + name + '\'' +
+                ", operationId='" + operationId + '\'' +
+                '}';
+    }
+}
