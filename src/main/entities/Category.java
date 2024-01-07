99a0aff9acd93d95a651028125418cbf2c1dfc5e
Marina <95050756+Marindala@users.noreply.github.com>
2024-01-07 01:31:00 -0300

    entities Category

--- src/main/entities/Category.java
+++ src/main/entities/Category.java
@@ -1,4 +1,52 @@
 package main.entities;
 
+
+
 public class Category {
+    private int id;
+    private String name;
+    private int operationTypeId;
+
+    public Category() {
+    }
+
+    public Category(int id, String name, int operationTypeId) {
+        this.id = id;
+        this.name = name;
+        this.operationTypeId = operationTypeId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOperationTypeId() {
+        return operationTypeId;
+    }
+
+    public void setOperationTypeId(int operationTypeId) {
+        this.operationTypeId = operationTypeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Category {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", operationTypeId='" + operationTypeId + '\'' +
+                '}';
+    }
 }
+
